cb506d728e4c71c081c59b9e52fd3c0284c55ff4
Alessandro Ristori <81309500+RistoAle97@users.noreply.github.com>
2021-04-23 19:17:31 +0200

    Update di app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from telegram.ext import *
-from telegram import Update, ForceReply
+from telegram import Update, ForceReply, ParseMode
 # import pymongo
 import logging
-import os
+# import os
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
@@ -10,18 +10,29 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 
-def start_command(update: Update, _: CallbackContext) -> None:
+def start_command(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /start is issued."""
     user = update.effective_user
+    # bot = context.bot
     update.message.reply_markdown_v2(
         fr'Hi {user.mention_markdown_v2()}\!',
         reply_markup=ForceReply(selective=True),
     )
 
 
-def help_command(update: Update, _: CallbackContext) -> None:
+def help_command(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
+    update.message.reply_text("List of ccommands:\n\n"
+                              "*Utils*\n"
+                              "/help - Shows a list of all possible commands\n"
+                              "/setup - Setup the bot for your needs",
+                              parse_mode=ParseMode.MARKDOWN)
+    # update.message.reply_text('Help!')
+
+
+def setup_command(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /help is issued."""
+    update.message.reply_text("Inserisci il nome col quale verrai registrato nel sistema")
 
 
 def main():
@@ -34,17 +45,18 @@ def main():
 
     dispatcher.add_handler(CommandHandler("start", start_command))
     dispatcher.add_handler(CommandHandler("help", help_command))
+    dispatcher.add_handler(CommandHandler("setup", setup_command))
 
-    # dispatcher.add_error_handler(None, error)
+    # dispatcher.add_error_handler(error)
 
     # Start the Bot
     updater.start_polling()
-    heroku_name = os.environ.get("HEROKU_APP_NAME")
-    updater.start_webhook(listen="0.0.0.0", port=port, url_path=token)
-    updater.bot.set_webhook("https://{0}.herokuapp.com/{1}".format(heroku_name, token))
+    # heroku_name = os.environ.get("HEROKU_APP_NAME")
+    # updater.start_webhook(listen="0.0.0.0", port=port, url_path=token)
+    # updater.bot.set_webhook("https://{0}.herokuapp.com/{1}".format(heroku_name, token))
     updater.idle()
 
-    updater.idle()
+    # updater.idle()
 
 
 if __name__ == '__main__':
